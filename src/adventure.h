ef6ab9742eaffaa6e5fb67b8c2584bf110d46493
cezary <cezary.bednarz@gmail.com>
2020-02-01 20:52:19 +0100

    fixed DP

--- src/adventure.h
+++ src/adventure.h
@@ -10,101 +10,103 @@
 #include "utils.h"
 
 class Adventure {
- public:
-  virtual ~Adventure() = default;
+public:
+    virtual ~Adventure() = default;
 
-  virtual uint64_t packEggs(std::vector<Egg> eggs, BottomlessBag& bag) = 0;
+    virtual uint64_t packEggs(std::vector<Egg> eggs, BottomlessBag& bag) = 0;
 
-  virtual void arrangeSand(std::vector<GrainOfSand>& grains) = 0;
+    virtual void arrangeSand(std::vector<GrainOfSand>& grains) = 0;
 
-  virtual Crystal selectBestCrystal(std::vector<Crystal>& crystals) = 0;
+    virtual Crystal selectBestCrystal(std::vector<Crystal>& crystals) = 0;
 };
 
 class LonesomeAdventure : public Adventure {
- public:
-  LonesomeAdventure() {}
+public:
+    LonesomeAdventure() {}
 
-  virtual uint64_t packEggs(std::vector<Egg> eggs, BottomlessBag& bag) {
-    int N = bag.getCapacity();
-    const uint64_t NO_ELEM = UINT64_MAX;
+    virtual uint64_t packEggs(std::vector<Egg> eggs, BottomlessBag& bag) {
+        int N = bag.getCapacity();
 
-    std::vector<uint64_t> DP(N + 1, NO_ELEM);
-    std::vector<std::pair<uint64_t, uint64_t >> retrieve(N + 1, std::make_pair(0, 0));
-    DP[0] = 0;
+        std::vector<uint64_t> DP(N + 1, 0);
+        std::vector<bool> possible(N + 1, false);
+        std::vector<std::pair<uint64_t, uint64_t >> retrieve(N + 1, std::make_pair(0, 0));
+        possible[0] = true;
 
-    for(auto &egg : eggs) {
-        auto size = egg.getSize();
-        auto weight = egg.getWeight();
+        for(auto &egg : eggs) {
+            auto size = egg.getSize();
+            auto weight = egg.getWeight();
+            for(int i = N; i >= 0; --i) {
+                if(i >= (int)size && possible[i - size]) {
+                    if (DP[i - size] + weight > DP[i]) {
+                        DP[i] = DP[i - size] + weight;
+                        retrieve[i] = {size, weight};
+                        possible[i] = true;
+                    }
+                }
+            }
+        }
+
         for(int i = N; i >= 0; --i) {
-            if(DP[i - size] != NO_ELEM) {
-                if (DP[i - size] + weight > DP[i]) {
-                    DP[i] = DP[i - size] + weight;
-                    retrieve[i] = {size, weight};
-                  }
-              }
-          }
-      }
+            if(possible[i]) {
+                int curr = i;
+                while(curr > 0) {
+                    bag.addEgg(Egg(retrieve[curr].first, retrieve[curr].second));
+                    curr -= retrieve[i].first;
+                }
+                return DP[i];
+            }
+        }
 
-    for(int i = N; i >= 0; --i) {
-        if(DP[i] != NO_ELEM) {
-            int curr = i;
-            while(curr > 0) {
-                bag.addEgg(Egg(retrieve[curr].first, retrieve[curr].second));
-                curr -= retrieve[i].first;
-              }
-            return DP[i];
-          }
-      }
+        return 0;
+    }
 
-    return -1; // error
-  }
+private:
+    static void merge_sort(std::vector<GrainOfSand>::iterator first, std::vector<GrainOfSand>::iterator last) {
+        if (last - first <= 1) {
+            return;
+        }
+        auto middle = first + (last - first) / 2;
+        merge_sort(first, middle);
+        merge_sort(middle, last);
+        std::inplace_merge(first, middle, last);
+    }
 
- private:
-  static void merge_sort(std::vector<GrainOfSand>::iterator first, std::vector<GrainOfSand>::iterator last) {
-    if (last - first <= 1) {
-        return;
-      }
-    auto middle = first + (last - first) / 2;
-    merge_sort(first, middle);
-    merge_sort(middle, last);
-    std::inplace_merge(first, middle, last);
-  }
- public:
+public:
+    virtual void arrangeSand(std::vector<GrainOfSand>& grains) {
+        auto first = grains.begin(), last = grains.end();
+        merge_sort(first, last);
+    }
 
-  virtual void arrangeSand(std::vector<GrainOfSand>& grains) {
-    auto first = grains.begin(), last = grains.end();
-    merge_sort(first, last);
-  }
-
-  virtual Crystal selectBestCrystal(std::vector<Crystal>& crystals) {
-    return *std::max_element(crystals.begin(), crystals.end());
-  }
+    virtual Crystal selectBestCrystal(std::vector<Crystal>& crystals) {
+        return *std::max_element(crystals.begin(), crystals.end());
+    }
 };
 
 class TeamAdventure : public Adventure {
- public:
-  explicit TeamAdventure(uint64_t numberOfShamansArg)
-      : numberOfShamans(numberOfShamansArg),
+public:
+
+    explicit TeamAdventure(uint64_t numberOfShamansArg)
+        : numberOfShamans(numberOfShamansArg),
         councilOfShamans(numberOfShamansArg) {}
 
-  uint64_t packEggs(std::vector<Egg> eggs, BottomlessBag& bag) {
-    LonesomeAdventure adventure;
-    return adventure.packEggs(eggs, bag);
-  }
+    uint64_t packEggs(std::vector<Egg> eggs, BottomlessBag& bag) {
+        LonesomeAdventure adventure;
+        return adventure.packEggs(eggs, bag);
+    }
 
-  virtual void arrangeSand(std::vector<GrainOfSand>& grains) {
-    LonesomeAdventure adventure;
-    adventure.arrangeSand(grains);
-  }
+    virtual void arrangeSand(std::vector<GrainOfSand>& grains) {
+        LonesomeAdventure adventure;
+        adventure.arrangeSand(grains);
+    }
 
-  virtual Crystal selectBestCrystal(std::vector<Crystal>& crystals) {
-    LonesomeAdventure adventure;
-    return adventure.selectBestCrystal (crystals);
-  }
+    virtual Crystal selectBestCrystal(std::vector<Crystal>& crystals) {
+        LonesomeAdventure adventure;
+        return adventure.selectBestCrystal (crystals);
+    }
 
- private:
-  uint64_t numberOfShamans;
-  ThreadPool councilOfShamans;
+private:
+    uint64_t numberOfShamans;
+    ThreadPool councilOfShamans;
 };
 
 #endif  // SRC_ADVENTURE_H_
