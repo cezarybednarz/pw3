f45cc96a627c20df68171723bf8bb5e99be9e94a
cezary <cezary.bednarz@gmail.com>
2020-02-02 23:00:20 +0100

    working on all tests, needs tidy

---
+++ src/tests/CMakeFiles/dp_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dp_test.dir/dp_test.cpp.o"
+  "dp_test.pdb"
+  "dp_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/tests/CMakeFiles/max_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/max_test.dir/max_test.cpp.o"
+  "max_test.pdb"
+  "max_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/max_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
